fcfec402633a15bb2a61f63783dc1371a0dd5bbc
Will <gdeletis@alteanet.it>
2022-04-19 21:39:38 +0200

    Starting project

---
+++ RickAndMortyAPI/RickAndMortyAPI/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  RickAndMortyAPI
+//
+//  Created by Guglielmo Deletis on 19/04/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ RickAndMortyAPI/RickAndMortyAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyAPI
+//
+//  Created by Guglielmo Deletis on 19/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
